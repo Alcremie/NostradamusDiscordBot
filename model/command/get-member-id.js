da47a46c26f81df888a7e19f2609d79f7937c74a
Jay Turner <jaywilliamturner@gmail.com>
2019-11-24 03:05:04 -0500

    Added get-member-id command

---
+++ model/command/get-member-id.js
@@ -0,0 +1,27 @@
+const Logger = require('@elian-wonhalf/pretty-logger');
+const Guild = require('../guild');
+
+/**
+ * @param {Message} message
+ */
+module.exports = {
+    aliases: ['getmemberid', 'memberid', 'gmid'],
+    process: async (message) => {
+        const member = await Guild.getMemberFromMessage(message);
+
+        if (Guild.isMemberMod(member)) {
+            const result = Guild.findDesignatedMemberInMessage(message);
+
+            if (result.foundMembers.count > 0) {
+            	message.channel.send(result.foundMembers[0]);
+            	message.channel.send(result.foundMembers[0].id);
+
+        	}
+
+        	else {
+        		message.reply(trans('model.command.getMemberId.notFound'))
+        		
+        	}
+        }
+    }
+};
